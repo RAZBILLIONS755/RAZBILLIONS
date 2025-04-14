863d3e3db43987d22b205406ee9beb34d25ad7d6
RAZBILLIONS755 <razbillions@gmail.com>
2025-04-14 16:18:28 -0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'razbillions',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "Razbillions bot",
+  botOwner: 'RAZBILLIONS', // Your name
+  ownerNumber: '599504341', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
