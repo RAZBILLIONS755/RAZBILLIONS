d1019a2ff1a83f00eccb1c2b3d8ef082dd98546c
RAZBILLIONS755 <razbillions@gmail.com>
2025-04-23 10:11:03 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: 'Bot',
   botName: "Razbillions bot",
   botOwner: 'RAZBILLIONS', // Your name
-  ownerNumber: '599504341', //Your number
+  ownerNumber: '+233599504341', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
